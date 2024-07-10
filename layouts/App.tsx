c3b0955a27b81a4c53721edcce860d24bc2c9c4a
Luoie8340 <itslukiee@gmail.com>
2024-07-11 01:38:57 +0800

    converted lumi spinning to framer-motion & deleted unused css files

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,12 +1,23 @@
+import { motion } from "framer-motion"
 import aboba from '/aboba.png'
-import '../styles/App.css'
 
 function App() {
   return (
-    <div className="flex w-full flex-col h-[100svh] items-center justify-center">
-      <a href="" target="_blank">
-        <img src={aboba} className="lumi lumi-spin react" alt="React logo" />
-      </a>
+    <div className="flex w-full flex-col h-[100svh] items-center
+      justify-center">
+      <motion.div
+        animate={{
+          rotate: 360
+        }}
+        transition={{
+          duration: 1,
+          repeat: Infinity,
+          ease: "linear"
+        }}
+        className="size-24">
+        <img src={aboba} alt="Spinning Lumi" />
+      </motion.div>
+
       <h1 className="text-4xl mt-4">lumi ah :3</h1>
     </div>
   )
